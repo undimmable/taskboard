ac2a034a11500c5cb53c6224efd35825a96d0832
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-17 16:35:11 +0300

    - Add dummy login_form

---
+++ src/app/view/templates/login_form.php
@@ -0,0 +1,25 @@
+<div class="signin-form">
+    <div class="container">
+        <form class="form-signin" method="post" id="login-form">
+            <h2 class="form-signin-heading">Log In to TaskBoard.</h2>
+            <hr/>
+            <div id="error"></div>
+            <div class="form-group">
+                <input type="hidden" id="back" name="back" value="<? $redirect_back ?>"/>
+                <input type="email" class="form-control" placeholder="Email address" name="user_email" id="user_email"/>
+                <span id="check-e"></span>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Password" name="password" id="password"/>
+            </div>
+            <hr/>
+            <div class="form-group">
+                <button type="submit" class="btn btn-default" name="btn-login" id="btn-login">
+                    <span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In
+                </button>
+            </div>
+        </form>
+
+    </div>
+
+</div>
