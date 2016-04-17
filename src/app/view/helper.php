494c091562e14ac7ee832ddd7eab4a04ea06ba06
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-17 16:33:51 +0300

    - Add templates for header/footer

---
+++ src/app/view/helper.php
@@ -0,0 +1,24 @@
+<?php
+function add_html_header()
+{
+    add_static_template('header', $_SERVER["CONTENT_TYPE"]);
+}
+
+function add_html_footer()
+{
+    add_static_template('footer', $_SERVER["CONTENT_TYPE"]);
+}
+
+function add_static_template($template_name, $content_type)
+{
+    if ($content_type !== "application/json") {
+        include "templates/$template_name.html";
+    }
+}
+
+function add_dynamic_template($template_name, $content_type)
+{
+    if ($content_type !== "application/json") {
+        include "templates/$template_name.php";
+    }
+}
