3e5d228d92c5352b02c8bc5c1c379021d8159ddd
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-18 08:14:41 +0300

    - Add db_config initializing

--- src/app/bootstrap.php
+++ src/app/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 require_once "lib/helper.php";
+require_once "config/db_config.php";
 require_once "security/force_https.php";
 require_once "security/jwt.php";
 require_once "security/token_auth.php";
---
+++ src/app/config/db_config.php
@@ -0,0 +1,2 @@
+<?php
+$db_config = parse_ini_file('/etc/php5/fpm/conf.d/db_config.ini', true);
