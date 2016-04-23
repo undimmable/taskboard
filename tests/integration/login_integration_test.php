f2eda0cf82b7b8e60d151c56b676caec2b0faaf9
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-24 00:06:46 +0300

    - Use mysqli instead of mysql  - Drop heroku buildpack support  - Add guzzlehttp

--- tests/integration/login_integration_test.php
+++ tests/integration/login_integration_test.php
@@ -1,10 +1,43 @@
 <?php
 
-class LoginIntegrationTest extends PHPUnit_Framework_TestCase
+use \GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use \GuzzleHttp\Psr7\Response;
+
+class LoginIntegrationTest extends \PHPUnit_Framework_TestCase
 {
-    public function testPlaceholder()
+    /**
+     * @var \GuzzleHttp\Client;
+     */
+    protected $api;
+
+    public function setUp()
     {
-        $a = 1;
-        $this->assertEquals(1, $a);
+        $this->api = new Client([
+            'base_uri' => 'https://taskboard.dev/api/v1/',
+            'timeout' => 0.25,
+            'cookies' => true,
+            'verify' => false
+        ]);
+    }
+
+    public function tearDown()
+    {
+        unset($this->api);
+    }
+
+
+    public function testLoginWithUsernamePasswordShouldReturnToken()
+    {
+        $credentials = ['username' => 'validuser', 'password' => 'validuserpassword'];
+        $response = $this->api->post('auth/login', ['form_params' => $credentials]);
+        print_r($response->getHeaders());
+        $body = $response->getBody();
+        if ($body->getSize() > 0) {
+            $rspSize = $body->getSize();
+            echo $body->read($rspSize);
+        } else
+            echo "Empty body";
+        $this->assertEquals($response->getStatusCode(), 200);
     }
 }
