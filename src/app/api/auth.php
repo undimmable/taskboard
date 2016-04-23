ea86362e26ed1828efca5fa6f1b64bb2ea6cb8e2
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-24 00:08:57 +0300

    - Remove render static/dynamic render regardless  - Render not found in auth instead of simply setting 404 code

--- src/app/api/auth.php
+++ src/app/api/auth.php
@@ -1,10 +1,27 @@
 <?php
+/**
+ * Login functions
+ *
+ * PHP version 5
+ *
+ * @category  APIFunctions
+ * @package   Api
+ * @author    Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
+ * @copyright 2016 Dmitry Bogdanov
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @version   GIT: $Id$ In development.
+ * @link      https://taskboards.top
+ * @since     1.0.0
+ */
+/**
+ * Bootstrap application
+ */
 require_once "../bootstrap.php";
 if ($_REQUEST["method"] === "login") {
     if (is_get()) {
         add_html_header();
         $redirect_back = "/";
-        add_dynamic_template("login_form", "text/html");
+        add_template("login_form", "text/html");
         add_html_footer();
     }
 
@@ -21,5 +38,5 @@ if ($_REQUEST["method"] === "login") {
         http_response_code(401);
     }
 } else {
-    http_response_code(404);
+    render_not_found();
 }
--- src/app/view/helper.php
+++ src/app/view/helper.php
@@ -1,24 +1,27 @@
 <?php
+
+function render_not_found($content_type = "text/html")
+{
+    http_response_code(404);
+    if ($content_type !== "application/json") {
+        include $_SERVER['DOCUMENT_ROOT'] . '/../public/html/http_errors/404.html';
+        die;
+    }
+}
+
 function add_html_header()
 {
-    add_static_template('header', $_SERVER["CONTENT_TYPE"]);
+    add_template('header', $_SERVER["CONTENT_TYPE"]);
 }
 
 function add_html_footer()
 {
-    add_static_template('footer', $_SERVER["CONTENT_TYPE"]);
+    add_template('footer', $_SERVER["CONTENT_TYPE"]);
 }
 
-function add_static_template($template_name, $content_type = "application/json")
+function add_template($template_name, $content_type = "application/json")
 {
     if ($content_type !== "application/json") {
-        include "templates/$template_name.html";
-    }
-}
-
-function add_dynamic_template($template_name, $content_type = "application/json")
-{
-    if ($content_type !== "application/json") {
-        include "templates/$template_name.php";
+        include "templates/$template_name.html.php";
     }
 }
