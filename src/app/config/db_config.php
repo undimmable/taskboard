aad84cccd1ee8c84bc5813a3348c7028a40785e1
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-24 00:04:46 +0300

    - Add test db-config

--- src/app/config/db_config.php
+++ src/app/config/db_config.php
@@ -1,2 +1,8 @@
 <?php
-$db_config = parse_ini_file('/etc/php5/fpm/conf.d/db_config.ini', true);
+
+function initialize_configuration($config_location = '/etc/php5/fpm/conf.d/db_config.ini')
+{
+    return parse_ini_file($config_location, true);
+}
+
+$db_config = initialize_configuration($config_location);
