0986ab76589b1ed88380bb23bf70ffd54c851d24
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-25 02:03:20 +0300

    - Add confirmation email

--- src/app/lib/mail.php
+++ src/app/lib/mail.php
@@ -1,6 +1,15 @@
 <?php
 
-function send_confirmation_email($email, $signup_token)
+function send_verification_confirmed_email($email)
+{
+    $to = $email;
+    $subject = 'Thanks for the registration on taskboards.top';
+    $message = 'Now you can use this link to access your profile https://taskboard.dev/profile';
+    $headers = 'From: signup@taskboards.top';
+    mail($to, $subject, $message, $headers);
+}
+
+function send_verification_request_email($email, $signup_token)
 {
     $to = $email;
     $subject = 'Confirm registration on taskboards.top';
