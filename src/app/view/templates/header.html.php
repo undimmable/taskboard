8f9ac0515528c038d5c858422dff6f8f3966c1ea
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-24 15:43:59 +0300

    - Remove bootstrap.min.theme from header  - Add IE-specific scripts

--- src/app/view/templates/header.html.php
+++ src/app/view/templates/header.html.php
@@ -8,20 +8,21 @@
 <!--[if IE 8]>
 <html class="lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!-->
-<html lang="en"> <!--<![endif]-->
+<html lang="en"><!--<![endif]-->
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php ${page_title} ?></title>
     <?php
     include 'icons.html.php'
     ?>
     <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
     <script type="application/javascript" src="/js/app.js"></script>
 </head>
 <body>
