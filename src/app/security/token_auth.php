084dc1c7407b86567bd542cd0a92ebb0270659ec
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-25 01:30:55 +0300

    - Fix JWTDecode (return array instead of object)  - Rename to underscore

--- src/app/security/token_auth.php
+++ src/app/security/token_auth.php
@@ -1,38 +1,81 @@
 <?php
-$jwt_key = 'key_bootstrap';
+$user = null;
 
-function setToken($token, $session = true)
+function create_token($email, $role, $id)
 {
     global $jwt_key;
+    return JWT_encode([get_username_assoc_key() => $email, get_role_assoc_key() => $role, "id" => $id], $jwt_key);
+}
+
+function set_token_cookie($jwt_token, $session = true)
+{
     $expire = $session ? 0 : time() + 60 * 60 * 24 * 365;
-    setcookie("PRIVATE-TOKEN", JWT_encode($token, $jwt_key), $expire, "/", null, true, true);
+    setcookie("PRIVATE-TOKEN", $jwt_token, $expire, "/", null, true, true);
 }
 
-function deleteToken()
+function delete_token_cookie()
 {
-    global $jwt_key;
-    $privateToken = $_COOKIE['PRIVATE-TOKEN'];
-    if (!is_null($privateToken)) {
-        $payload = JWT_decode($privateToken, $jwt_key);
-        if (!is_null($payload)) {
-            setcookie("PRIVATE-TOKEN", null, 0, "/", null, true, true);
-            return true;
-        }
-    }
-    return false;
+    if (is_authorized()) {
+        setcookie(get_token_assoc_key(), null, 0, "/", null, true, true);
+        return true;
+    } else
+        return false;
 }
 
-function getToken()
+function parse_token_from_cookie()
 {
     global $jwt_key;
-    $privateToken = $_COOKIE['PRIVATE-TOKEN'];
+    $privateToken = $_COOKIE[get_token_assoc_key()];
     if (is_null($privateToken))
         return null;
     return JWT_decode($privateToken, $jwt_key);
 }
 
-function isAuthorized()
+function try_authorize_from_cookie()
 {
-    $c = getToken() !== null;
-    return $c;
+    if (!is_null(get_authorized_user()))
+        return;
+    $token = parse_token_from_cookie();
+    if (is_null($token))
+        return;
+    set_authorized_user(parse_user_from_token($token));
+}
+
+function parse_user_from_token($token)
+{
+    if (!array_key_exists(get_username_assoc_key(), $token) || !array_key_exists(get_role_assoc_key(), $token) || !array_key_exists('id', $token))
+        return null;
+    $username = $token[get_username_assoc_key()];
+    if (is_null($username))
+        return null;
+    $role = $token[get_role_assoc_key()];
+    if (is_null($role))
+        return null;
+    $id = $token['id'];
+    if (is_null($id))
+        return null;
+    return [get_username_assoc_key() => $username, get_role_assoc_key() => $role, 'id' => $id];
+}
+
+function get_authorized_user()
+{
+    global $user;
+    return $user;
+}
+
+function set_authorized_user($authorized_user)
+{
+    global $user;
+    $user = $authorized_user;
+}
+
+function is_authorized()
+{
+    return !is_null(get_authorized_user());
+}
+
+function create_confirmation_token($string)
+{
+    global $confirmation_key;
+    return hash("sha256", $string . $confirmation_key);
 }
