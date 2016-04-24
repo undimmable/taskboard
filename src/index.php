d50caf79a965dd21eefb88e344982d42283de055
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-25 02:32:18 +0300

    - Render login form on index

--- src/index.php
+++ src/index.php
@@ -1,7 +1,10 @@
 <?php
 require_once "app/bootstrap.php";
-if (!isAuthorized()) {
-    https_redirect("/api/v1/auth/login", "/feed");
+if (!is_authorized()) {
+    add_html_header();
+    add_html_navbar();
+    add_login_form();
+    add_html_footer();
     die();
 } else {
     https_redirect("/feed");
