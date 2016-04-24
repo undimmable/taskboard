746372ea30d70bbae9bd827b3c6148f134d0ab84
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-25 01:44:20 +0300

    - Add user_mail, password and role to login

--- config/db/login.sql
+++ config/db/login.sql
@@ -3,9 +3,13 @@ CREATE DATABASE db_login;
 GRANT ALL ON db_login.* TO 'user_login'@'%';
 USE db_login;
 CREATE TABLE login (
-  id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-  user_id    BIGINT                  NOT NULL,
-  created_at TIMESTAMP DEFAULT now() NOT NULL,
-  token      VARCHAR(255)
+  id                 BIGINT PRIMARY KEY               AUTO_INCREMENT,
+  user_id            BIGINT                  NOT NULL UNIQUE,
+  created_at         TIMESTAMP DEFAULT now() NOT NULL,
+  user_email         VARCHAR(255)            NOT NULL UNIQUE,
+  password           VARCHAR(255)            NOT NULL,
+  role               TINYINT                 NOT NULL,
+  confirmation_token VARCHAR(255)            NOT NULL,
+  confirmed          BOOLEAN                 NOT NULL DEFAULT FALSE
 )
-  ENGINE = InnoDB;
+ENGINE = InnoDB;
