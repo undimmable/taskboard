ac6649c8dcd8c602d22b70b10ca12f9d58ab7088
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-24 12:14:17 +0300

    - Feed page

--- src/index.php
+++ src/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once "app/bootstrap.php";
 if (!isAuthorized()) {
-    https_redirect("/api/v1/auth/login", "/");
+    https_redirect("/api/v1/auth/login", "/feed");
     die();
+} else {
+    https_redirect("/feed");
 }
