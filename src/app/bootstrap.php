95d6bc07f4a068552db11fff451eeca58350ca13
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-25 02:02:55 +0300

    - Check authorization in bootstrap

--- src/app/bootstrap.php
+++ src/app/bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 require_once "lib/helper.php";
+require_once "lib/validation.php";
 require_once "lib/mobile.php";
+require_once "config/constants.php";
 require_once "config/db_config.php";
+require_once "config/security_config.php";
+require_once "config/role.php";
 require_once "security/force_https.php";
 require_once "security/jwt.php";
 require_once "security/token_auth.php";
 require_once "view/helper.php";
+
+try_authorize_from_cookie();
