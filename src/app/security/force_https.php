7b6e2f44c23bc5387c29b903455ee729d2a9f464
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-25 01:32:31 +0300

    - Include subdomains in force_https

--- src/app/security/force_https.php
+++ src/app/security/force_https.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_SERVER['REQUEST_URI'])) {
     if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
-        header('Strict-Transport-Security: max-age=31536000');
+        header('Strict-Transport-Security: max-age=31536000; includeSubdomains;');
     } else {
         $uri = $_SERVER['REQUEST_URI'];
         https_redirect($uri);
