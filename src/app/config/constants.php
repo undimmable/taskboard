43a48219300bd86cbfee0f2df05059bd4c657694
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-04-25 01:43:57 +0300

    - Add constants for associative arrays keys

---
+++ src/app/config/constants.php
@@ -0,0 +1,21 @@
+<?php
+
+function get_username_assoc_key()
+{
+    return 'user_email';
+}
+
+function get_password_assoc_key()
+{
+    return 'user_password';
+}
+
+function get_role_assoc_key()
+{
+    return 'user_role';
+}
+
+function get_token_assoc_key()
+{
+    return 'PRIVATE-TOKEN';
+}
