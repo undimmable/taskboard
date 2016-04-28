65647ebdf39228d268970bae04cca66f309c9123
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-04-29 00:44:53 +0300

    - Move constants on top  - Do not set php_config_path if it's been already set

--- src/app/bootstrap.php
+++ src/app/bootstrap.php
@@ -1,8 +1,12 @@
 <?php
+if (!isset($php_config_path)) {
+    $php_config_path = "/var/www/taskboard_config";
+};
+require_once "config/constants.php";
 require_once "lib/helper.php";
 require_once "lib/validation.php";
 require_once "lib/mobile.php";
-require_once "config/constants.php";
+require_once "lib/router.php";
 require_once "config/db_config.php";
 require_once "config/security_config.php";
 require_once "config/role.php";
