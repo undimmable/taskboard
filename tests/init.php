10caad52f4dc3b9848b74d2dd5c7407e1904bba4
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-04-29 00:49:41 +0300

    - Do not require bootstrap in tests

--- tests/init.php
+++ tests/init.php
@@ -1,4 +1,3 @@
 <?php
-$config_location = realpath("./config/db/db_config.test.ini");
-require_once 'vendor/autoload.php';
-require_once "src/app/bootstrap.php";
+$php_config_path = 'tests/config';
+require_once "vendor/autoload.php";
