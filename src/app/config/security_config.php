6adc4658a89cd8957f7bd056963e0de677a04cca
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-04-29 00:47:06 +0300

    - Extract jwt and the like in config

--- src/app/config/security_config.php
+++ src/app/config/security_config.php
@@ -1,5 +1,36 @@
 <?php
+$key_config = null;
 
-$jwt_key = 'key_bootstrap';
-$confirmation_key = 'confirmation_key_bootstrap';
-$minimal_password_length = 6;
+function get_key_config()
+{
+    global $php_config_path, $key_config;
+    if (is_null($key_config)) {
+        $key_config = include "$php_config_path/taskboard_key_config.php";
+    }
+    return $key_config;
+}
+
+function get_key_jwt_secret()
+{
+    return get_key_config()['jwt_secret'];
+}
+
+function get_key_confirmation_secret()
+{
+    return get_key_config()['confirmation_secret'];
+}
+
+function get_key_vk_client_id()
+{
+    return get_key_config()['vk_client_id'];
+}           
+
+function get_key_vk_secret()
+{
+    return get_key_config()['vk_secret'];
+}
+
+function get_minimal_password_length()
+{
+    return get_key_config()['minimal_password_length'];
+}
