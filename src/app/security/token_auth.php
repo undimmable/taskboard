b3670c433e5adea881ec0da1fd2ccce665dd6f3f
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-04-29 00:45:40 +0300

    - Use constants in JWT

--- src/app/security/token_auth.php
+++ src/app/security/token_auth.php
@@ -3,20 +3,19 @@ $user = null;
 
 function create_token($email, $role, $id)
 {
-    global $jwt_key;
-    return JWT_encode([get_username_assoc_key() => $email, get_role_assoc_key() => $role, "id" => $id], $jwt_key);
+    return JWT_encode([USER_EMAIL => $email, USER_ROLE => $role, ID => $id], get_key_jwt_secret());
 }
 
 function set_token_cookie($jwt_token, $session = true)
 {
     $expire = $session ? 0 : time() + 60 * 60 * 24 * 365;
-    setcookie("PRIVATE-TOKEN", $jwt_token, $expire, "/", null, true, true);
+    setcookie(PRIVATE_TOKEN, $jwt_token, $expire, "/", null, true, true);
 }
 
 function delete_token_cookie()
 {
     if (is_authorized()) {
-        setcookie(get_token_assoc_key(), null, 0, "/", null, true, true);
+        setcookie(PRIVATE_TOKEN, null, 0, "/", null, true, true);
         return true;
     } else
         return false;
@@ -24,11 +23,10 @@ function delete_token_cookie()
 
 function parse_token_from_cookie()
 {
-    global $jwt_key;
-    $privateToken = $_COOKIE[get_token_assoc_key()];
+    $privateToken = $_COOKIE[PRIVATE_TOKEN];
     if (is_null($privateToken))
         return null;
-    return JWT_decode($privateToken, $jwt_key);
+    return JWT_decode($privateToken, get_key_jwt_secret());
 }
 
 function try_authorize_from_cookie()
@@ -43,18 +41,18 @@ function try_authorize_from_cookie()
 
 function parse_user_from_token($token)
 {
-    if (!array_key_exists(get_username_assoc_key(), $token) || !array_key_exists(get_role_assoc_key(), $token) || !array_key_exists('id', $token))
+    if (!array_key_exists(USER_EMAIL, $token) || !array_key_exists(USER_ROLE, $token) || !array_key_exists(ID, $token))
         return null;
-    $username = $token[get_username_assoc_key()];
+    $username = $token[USER_EMAIL];
     if (is_null($username))
         return null;
-    $role = $token[get_role_assoc_key()];
+    $role = $token[USER_ROLE];
     if (is_null($role))
         return null;
-    $id = $token['id'];
+    $id = $token[ID];
     if (is_null($id))
         return null;
-    return [get_username_assoc_key() => $username, get_role_assoc_key() => $role, 'id' => $id];
+    return [USER_EMAIL => $username, USER_ROLE => $role, ID => $id];
 }
 
 function get_authorized_user()
@@ -76,6 +74,5 @@ function is_authorized()
 
 function create_confirmation_token($string)
 {
-    global $confirmation_key;
-    return hash("sha256", $string . $confirmation_key);
+    return hash("sha256", $string . get_key_confirmation_secret());
 }
