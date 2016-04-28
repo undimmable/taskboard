0f26f86d0cd9c37a4be61d5edb466b2b756ef8d1
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-04-29 00:44:15 +0300

    - Add simple router

--- src/app/api/auth.php
+++ src/app/api/auth.php
@@ -20,103 +20,96 @@ require_once "../bootstrap.php";
 require_once "../lib/mail.php";
 require_once "../dal/user.php";
 
-function validate_input($email, $role, $password)
+$routes = [
+    'POST' => [
+        'login' => 'auth_login_action',
+        'logout' => 'auth_logout_action',
+        'signup' => 'auth_signup_action',
+    ],
+    'GET' => [
+        'verify' => 'auth_verify_action',
+        'signup_vk' => 'auth_signup_vk_action'
+    ],
+    'PUT' => [],
+    'DELETE' => []
+];
+
+function __validate_signup_input($email, $role, $password)
 {
-    initialize_user_input_validation();
-    is_email_valid($email);
-    is_role_valid($role);
-    is_password_valid($password);
-    if (user_input_has_errors()) {
-        echo json_encode(get_user_input_validation_errors());
+    $validation_context = initialize_validation_context();
+    is_email_valid($email, $validation_context);
+    is_role_valid($role, $validation_context);
+    is_password_valid($password, $validation_context);
+    if (validation_context_has_errors($validation_context)) {
+        echo json_encode(get_all_validation_errors($validation_context));
         die;
     }
 }
 
-function validate_login_input($email, $role)
+function __validate_login_input($email, $role)
 {
-    initialize_user_input_validation();
-    is_email_valid($email);
-    is_role_valid($role);
-    if (user_input_has_errors()) {
-        echo json_encode(get_user_input_validation_errors());
+    $validation_errors = initialize_validation_context();
+    is_email_valid($email, $validation_errors);
+    is_role_valid($role, $validation_errors);
+    if (validation_context_has_errors($validation_errors)) {
+        echo json_encode(get_all_validation_errors($validation_errors));
         die;
     }
 }
 
-function is_login_action()
-{
-    return $_REQUEST["method"] === "login";
-}
-
-function is_signup_action()
-{
-    return $_REQUEST["method"] === "signup";
-}
-
-function is_verify_action()
-{
-    return $_REQUEST["method"] === "verify";
-}
-
-function is_logout_action()
-{
-    return $_REQUEST["method"] === "logout";
-}
-
-function process_login_action()
+function auth_login_action()
 {
     if (is_request_www_form()) {
-        $email = $_POST[get_username_assoc_key()];
-        $password = $_POST[get_password_assoc_key()];
+        $data = $_POST;
     } elseif (is_request_json()) {
         $data = json_decode(file_get_contents('php://input'), true);
-        $email = $data[get_username_assoc_key()];
-        $password = $data[get_password_assoc_key()];
     } else {
         render_unsupported_media_type();
         die;
     }
-    $user = get_user_by_email($email);
+    $email = $data["email"];
+    $password = $data["password"];
+    $user = db_fetch_user_by_email($email);
     if ($user === null)
         render_not_found();
-    $hashed_password = $user[get_password_assoc_key()];
+    $hashed_password = $user["hashed_password"];
     if (!password_verify($password, $hashed_password))
         render_not_found();
-    $token = create_token($email, $user[get_username_assoc_key()], $user['id']);
+    $token = create_token($user["email"], $user["role"], $user['id']);
     set_token_cookie($token);
     render_ok();
 }
 
-function process_signup_action()
+function auth_signup_action()
 {
     if (is_request_www_form()) {
-        $email = $_POST[get_username_assoc_key()];
-        $role = $_POST[get_role_assoc_key()];
-        $password = $_POST[get_password_assoc_key()];
+        $email = $_POST[USER_EMAIL];
+        $role = $_POST[USER_ROLE];
+        $password = $_POST[USER_PASSWORD];
     } elseif (is_request_json()) {
         $data = json_decode(file_get_contents('php://input'), true);
-        $email = $data[get_username_assoc_key()];
-        $role = $data[get_role_assoc_key()];
-        $password = $data[get_password_assoc_key()];
+        $email = $data[USER_EMAIL];
+        $role = $data[USER_ROLE];
+        $password = $data[USER_PASSWORD];
     } else {
         render_unsupported_media_type();
         die;
     }
-    validate_input($email, $role, $password);
+    __validate_signup_input($email, $role, $password);
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     $confirmation_token = create_confirmation_token($email);
     $user = create_user($email, $role, $hashed_password, $confirmation_token);
     if (!$user) {
         $db_errors = get_db_errors();
-        render_conflict($db_errors[get_login_entity_name()]);
+        render_conflict($db_errors[LOGIN]);
     }
-    $token = create_token($email, $role, $user['id']);
+    $token = create_token($email, $role, $user[ID]);
     set_token_cookie($token);
     send_verification_request_email($email, $confirmation_token);
     flush();
 }
 
-function process_logout_action()
+function auth_logout_action()
 {
     if (delete_token_cookie())
         render_ok();
@@ -124,12 +117,12 @@ function process_logout_action()
         render_not_authorized();
 }
 
-function process_verify_action()
+function auth_verify_action()
 {
     if (isset($_GET['confirmation_token'])) {
         $user = verify_user($_GET['confirmation_token']);
         if (!is_null($user)) {
-            send_verification_confirmed_email($user[get_username_assoc_key()]);
+            send_verification_confirmed_email($user[USER_EMAIL]);
             https_redirect("/");
         } else
             render_not_authorized();
@@ -138,33 +131,4 @@ function process_verify_action()
     }
 }
 
-function is_action_allowed()
-{
-    if (!is_post()) {
-        return is_get() && is_verify_action();
-    }
-    if (is_authorized())
-        return is_logout_action();
-    else
-        return is_login_action() || is_signup_action();
-}
-
-function process_request()
-{
-    if (!is_action_allowed()) {
-        render_not_allowed();
-    }
-    if (is_login_action()) {
-        process_login_action();
-    } elseif (is_signup_action()) {
-        process_signup_action();
-    } elseif (is_logout_action()) {
-        process_logout_action();
-    } elseif (is_verify_action()) {
-        process_verify_action();
-    } else {
-        render_not_allowed();
-    }
-}
-
-process_request();
+route_request($routes);
---
+++ src/app/lib/router.php
@@ -0,0 +1,25 @@
+<?php
+
+function route_request($routes)
+{
+    $request_method = $_SERVER["REQUEST_METHOD"];
+    if (is_null($request_method) || !isset($routes[$request_method]))
+        render_not_allowed();
+    $path_param = $_REQUEST['path_param'];
+
+    if (!is_null($path_param)) {
+        if (array_key_exists($path_param, $routes[$request_method])) {
+            if (function_exists($routes[$request_method][$path_param])) {
+                return call_user_func($routes[$request_method][$path_param]);
+            }
+        } else if (array_key_exists('/\d+/', $routes[$request_method])) {
+            if (preg_match('/\d+/', $path_param)) {
+                $id = $path_param;
+                if (function_exists($routes[$request_method]['/\d+/']))
+                    return call_user_func($routes[$request_method]['/\d+/'], $id);
+            }
+        }
+    }
+    render_not_found();
+    die;
+}
