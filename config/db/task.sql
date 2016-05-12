34599c702837c4b29bc6c77dc2b2f4d9a9f676a5
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 11:21:01 +0300

    - Add task amount

--- config/db/task.sql
+++ config/db/task.sql
@@ -5,8 +5,8 @@ USE db_task;
 CREATE TABLE task (
   id           BIGINT PRIMARY KEY AUTO_INCREMENT,
   created_at   TIMESTAMP DEFAULT now() NOT NULL,
-  updated_at   TIMESTAMP,
   customer_id  BIGINT                  NOT NULL,
-  performer_id BIGINT             DEFAULT NULL
+  performer_id BIGINT             DEFAULT NULL,
+  amount       NUMERIC(10, 2)          NOT NULL
 )
   ENGINE = InnoDB;
