a62f7bf5a334cc9ab9be2cf5df4c559c619c4db5
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 12:11:38 +0300

    - Send html-emails

--- src/app/lib/mail.php
+++ src/app/lib/mail.php
@@ -1,19 +1,29 @@
 <?php
 
-function send_verification_confirmed_email($email)
+function send_verification_confirmed_email($email, $host)
 {
     $to = $email;
     $subject = 'Thanks for the registration on taskboards.top';
-    $message = 'Now you can use this link to access your profile https://taskboard.dev/profile';
-    $headers = 'From: signup@taskboards.top';
+    $message = '<html><body>';
+    $message .= 'Now you can use <a href="https://' . $host . '/profile">this link</a> to access your profile ';
+    $message .= '</body></html>';
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $headers .= 'From: Taskboards <signup@taskboards.top>' . "\r\n";
+    $headers .= 'Reply-To: Taskboards <signup@taskboards.top>' . "\r\n";
     mail($to, $subject, $message, $headers);
 }
 
-function send_verification_request_email($email, $signup_token)
+function send_verification_request_email($email, $host, $signup_token)
 {
     $to = $email;
     $subject = 'Confirm registration on taskboards.top';
-    $message = 'Follow this link to confirm signup request: https://taskboard.dev/api/v1/auth/verify?confirmation_token=' . $signup_token;
-    $headers = 'From: signup@taskboards.top';
+    $message = '<html><body>';
+    $message .= '<a href="https://' . $host . '/api/v1/auth/verify?confirmation_token=' . $signup_token . '">Follow this link to confirm signup request.</a>';
+    $message .= '</body></html>';
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $headers .= 'From: Taskboards <signup@taskboards.top>' . "\r\n";
+    $headers .= 'Reply-To: Taskboards <signup@taskboards.top>' . "\r\n";
     mail($to, $subject, $message, $headers);
 }
