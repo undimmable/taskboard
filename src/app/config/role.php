771c511f03b5f7981e94cb7e5dce9d88035da4de
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 12:08:07 +0300

    - Refactor roles

--- src/app/config/role.php
+++ src/app/config/role.php
@@ -1,15 +1,36 @@
 <?php
 
-$roles = array(1 => "system", 2 => "customer", 3 => "performer");
+$roles = array(1 => "System", 2 => "Customer", 3 => "Performer");
+$roles_reversed = array("System" => 1, "Customer" => 2, "Performer" => 3);
 
-function get_role($key)
+function get_role_value($key)
 {
     global $roles;
     return $roles[$key];
 }
 
-function role_exists($key)
+function get_role_key($role)
+{
+    global $roles_reversed;
+    return $roles_reversed[$role];
+}
+
+function role_key_exists($key)
 {
     global $roles;
     return array_key_exists($key, $roles);
+}
+
+function role_value_exists($key)
+{
+    global $roles;
+    return array_key_exists($key, $roles);
+}
+
+function is_customer($key) {
+    return $key === 2;
+}
+
+function is_performer($key) {
+    return $key === 3;
 }
