b6e1d40b1d48d71003bf8b74021f91e288338c53
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 11:21:10 +0300

    - Add login info

--- config/db/login.sql
+++ config/db/login.sql
@@ -3,13 +3,9 @@ CREATE DATABASE db_login;
 GRANT ALL ON db_login.* TO 'user_login'@'%';
 USE db_login;
 CREATE TABLE login (
-  id                 BIGINT PRIMARY KEY               AUTO_INCREMENT,
-  user_id            BIGINT                  NOT NULL UNIQUE,
-  created_at         TIMESTAMP DEFAULT now() NOT NULL,
-  user_email         VARCHAR(255)            NOT NULL UNIQUE,
-  password           VARCHAR(255)            NOT NULL,
-  role               TINYINT                 NOT NULL,
-  confirmation_token VARCHAR(255)            NOT NULL,
-  confirmed          BOOLEAN                 NOT NULL DEFAULT FALSE
+  user_id     BIGINT PRIMARY KEY,
+  ip          VARBINARY(16),
+  user_client VARCHAR(31),
+  created_at  TIMESTAMP
 )
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
