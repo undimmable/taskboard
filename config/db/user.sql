0cada0948b5e03a6dd90992d6bef956d9742f086
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 11:20:34 +0300

    - Add user table

---
+++ config/db/user.sql
@@ -0,0 +1,14 @@
+USE mysql;
+CREATE DATABASE db_user;
+GRANT ALL ON db_user.* TO 'user_user'@'%';
+USE db_user;
+CREATE TABLE user (
+  id                 BIGINT PRIMARY KEY UNIQUE               AUTO_INCREMENT,
+  created_at         TIMESTAMP DEFAULT now() NOT NULL,
+  email              VARCHAR(255)            NOT NULL UNIQUE,
+  hashed_password    VARCHAR(255)            NOT NULL,
+  role               TINYINT                 NOT NULL,
+  confirmation_token VARCHAR(255)            NOT NULL,
+  confirmed          BOOLEAN                 NOT NULL        DEFAULT FALSE
+)
+  ENGINE = InnoDB;
