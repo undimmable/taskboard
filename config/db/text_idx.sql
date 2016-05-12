b53038f2d3418239f213f2371e42c1e0272c55ff
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 11:20:50 +0300

    - Add text index

--- config/db/text_idx.sql
+++ config/db/text_idx.sql
@@ -4,10 +4,10 @@ GRANT ALL ON db_text_idx.* TO 'user_text_idx'@'%';
 USE db_text_idx;
 
 CREATE TABLE text_idx (
-  FTS_DOC_ID BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  entity_type       VARCHAR(10)                    NOT NULL,
-  entity_id  BIGINT                         NOT NULL,
-  text_val   TEXT                           NOT NULL,
+  FTS_DOC_ID  BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  entity_type VARCHAR(10)                    NOT NULL,
+  entity_id   BIGINT                         NOT NULL,
+  text_val    TEXT                           NOT NULL,
   FULLTEXT idx (text_val)
 )
   ENGINE = InnoDB;
