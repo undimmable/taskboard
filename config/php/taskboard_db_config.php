368991d682bcac2ab786eb67b5140de63c3892e1
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 11:22:39 +0300

    - Change db configuration according to new changes

--- config/php/taskboard_db_config.php
+++ config/php/taskboard_db_config.php
@@ -1,26 +1,5 @@
 <?php
 return [
-    'task' => [
-        'host' => 'rplc_task_host',
-        'port' => 3306,
-        'user' => 'user_task',
-        'password' => 'rplc_task_password',
-        'database' => 'db_task'
-    ],
-    'performer' => [
-        'host' => 'rplc_performer_host',
-        'port' => 3306,
-        'user' => 'user_performer',
-        'password' => 'rplc_performer_password',
-        'database' => 'db_performer'
-    ],
-    'customer' => [
-        'host' => 'rplc_customer_host',
-        'port' => 3306,
-        'user' => 'user_customer',
-        'password' => 'rplc_customer_password',
-        'database' => 'db_customer'
-    ],
     'account' => [
         'host' => 'rplc_account_host',
         'port' => 3306,
@@ -28,18 +7,46 @@ return [
         'password' => 'rplc_account_password',
         'database' => 'db_account'
     ],
-    'system' => [
-        'host' => 'rplc_system_host',
-        'port' => 3306,
-        'user' => 'user_system',
-        'password' => 'rplc_system_password',
-        'database' => 'db_system'
-    ],
     'login' => [
         'host' => 'rplc_login_host',
         'port' => 3306,
         'user' => 'user_login',
         'password' => 'rplc_login_password',
-        'database' => 'db_login',
+        'database' => 'db_login'
+    ],
+    'task' => [
+        'host' => 'rplc_task_host',
+        'port' => 3306,
+        'user' => 'user_task',
+        'password' => 'rplc_task_password',
+        'database' => 'db_task'
+    ],
+    'text_idx' => [
+        'host' => 'rplc_text_idx_host',
+        'port' => 3306,
+        'user' => 'user_text_idx',
+        'password' => 'rplc_text_idx_password',
+        'database' => 'db_text_idx'
+    ],
+    'tx' => [
+        'host' => 'rplc_tx_host',
+        'port' => 3306,
+        'user' => 'user_tx',
+        'password' => 'rplc_tx_password',
+        'database' => 'db_tx'
+    ],
+    'user' => [
+        'host' => 'rplc_user_host',
+        'port' => 3306,
+        'user' => 'user_user',
+        'password' => 'rplc_user_password',
+        'database' => 'db_user'
+    ],
+    'user_info' => [
+        'host' => 'rplc_user_info_host',
+        'port' => 3306,
+        'user' => 'user_user_info',
+        'password' => 'rplc_user_info_password',
+        'database' => 'db_user_info'
     ]
 ];
