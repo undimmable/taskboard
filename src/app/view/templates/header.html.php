cc53dd7fc00a0b275892131e30a7e44e28cfca56
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 12:31:44 +0300

    - Add bootstrap and jquery to header  - Add page_title to header

--- src/app/view/templates/header.html.php
+++ src/app/view/templates/header.html.php
@@ -12,8 +12,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php ${page_title} ?></title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <title><?php echo $page_title ?></title>
     <?php
     include 'icons.html.php'
     ?>
@@ -22,7 +22,13 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/style.css">
+    <script type="application/javascript" src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+            crossorigin="anonymous"></script>
+    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
     <script type="application/javascript" src="/js/app.js"></script>
 </head>
 <body>
