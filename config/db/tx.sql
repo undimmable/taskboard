7462d111ed616b398299a116983cb91985a9f095
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 10:35:42 +0300

    - Add transactions table

---
+++ config/db/tx.sql
@@ -0,0 +1,14 @@
+USE mysql;
+CREATE DATABASE db_tx;
+GRANT ALL ON db_tx.* TO 'user_tx'@'%';
+USE db_tx;
+
+CREATE TABLE tx (
+  id           BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  from_user_id BIGINT                            NOT NULL,
+  to_user_id   BIGINT                            NOT NULL,
+  amount       NUMERIC(10, 2),
+  INDEX (from_user_id, id),
+  INDEX (to_user_id, id)
+)
+  ENGINE = InnoDB;
