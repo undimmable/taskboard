0c08e25dd931eab13f63df3f347865d27763bbd7
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-12 12:12:42 +0300

    - Add page_title to index.php  - Remove login_form rendering from api

--- src/index.php
+++ src/index.php
@@ -1,9 +1,9 @@
 <?php
 require_once "app/bootstrap.php";
+$page_title = "index";
 if (!is_authorized()) {
     add_html_header();
     add_html_navbar();
-    add_login_form();
     add_html_footer();
     die();
 } else {
