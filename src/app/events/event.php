bdffb4b242b3c9784f40bd1d20bbdb90791c65b9
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-15 16:41:51 +0300

    - Add noop event

---
+++ src/app/events/event.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author dimyriy
+ * @version 1.0
+ */
+
+/**
+ * @param $entity_id        integer Id of an entity
+ * @param $json             string Event object
+ */
+function send_event($entity_id, $json)
+{
+    //do nothing
+}
