e3aa5ec22e0e5772bd0161451e1b95bab862ec88
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-25 01:34:54 +0300

    - Add task api

--- src/app/api/task.php
+++ src/app/api/task.php
@@ -1,6 +1,144 @@
 <?php
-if ($_REQUEST["method"] === "root") {
-    echo "root";
-} else {
-    echo $_REQUEST["method"];
+
+require_once "../bootstrap.php";
+require_once "../dal/task.php";
+
+$routes = [
+    POST => [
+        ROOT => 'api_task_create'
+    ],
+    GET => [
+        ROOT => 'api_task_get_last_n',
+        '/\d+/' => 'api_task_get_by_id'
+    ],
+    PUT => [
+        '/\d+/' => 'api_task_update_by_id'
+    ],
+    DELETE => [
+        '/\d+/' => 'api_task_delete_by_id'
+    ]
+];
+
+
+function __validate_amount($amount, &$validation_context)
+{
+    if (is_null($amount)) {
+        add_validation_error($validation_context, AMOUNT, 'Price not provided');
+        return false;
+    }
+    $amount = filter_var($amount, FILTER_VALIDATE_INT);
+    if (!$amount) {
+        add_validation_error($validation_context, AMOUNT, 'Is not a valid number');
+        return false;
+    }
+    if ($amount < get_config_min_amount()) {
+        add_validation_error($validation_context, AMOUNT, 'Price cannot be less than ' . get_config_min_amount());
+        return false;
+    }
+    if ($amount > get_config_max_amount()) {
+        add_validation_error($validation_context, AMOUNT, 'Price cannot be larger than ' . get_config_max_amount());
+        return false;
+    }
+    return true;
 }
+
+function __validate_description($description, &$validation_context)
+{
+    if (is_null($description) || strlen($description) < 1) {
+        add_validation_error($validation_context, DESCRIPTION, 'Description not provided');
+        return false;
+    }
+    return true;
+}
+
+function __validate_task_input($amount, $description)
+{
+    $validation_context = initialize_validation_context();
+    __validate_amount($amount, $validation_context);
+    __validate_description($description, $validation_context);
+    if (validation_context_has_errors($validation_context)) {
+        render_bad_request_json(['error' => get_all_validation_errors($validation_context)]);
+        die;
+    }
+}
+
+function api_task_get_by_id($task_id)
+{
+    $validation_context = initialize_validation_context();
+    $task_id = filter_var($task_id, FILTER_VALIDATE_INT);
+    $task = dal_task_fetch($task_id);
+    if ($task === null) {
+        render_not_found();
+        die;
+    } elseif (!$task) {
+        render_bad_request_json(['error' => get_all_validation_errors($validation_context)]);
+        die;
+    } else {
+        render_ok_json($task);
+    }
+}
+
+function api_task_get_last_n()
+{
+    if (!is_authorized())
+        render_not_authorized_json();
+    $user = get_authorized_user();
+    $tasks = dal_task_fetch_all_tasks($user[ID], $user[ROLE], parse_integer_param('limit'), parse_integer_param('last_id'));
+    if ($tasks === false) {
+        render_bad_request_json($tasks);
+    } else {
+        render_ok_json($tasks);
+    }
+}
+
+function api_task_update_by_id($id, $task)
+{
+    echo $task;
+    echo $id;
+}
+
+function api_task_create()
+{
+    if (!is_authorized())
+        render_not_authorized_json();
+    global $user;
+    if (is_request_www_form()) {
+        $data = $_POST;
+    } elseif (is_request_json()) {
+        $data = json_decode(file_get_contents('php://input'), true);
+    } else {
+        render_unsupported_media_type();
+        die;
+    }
+    $customer_id = $user[ID];
+    $amount = $data[AMOUNT];
+    $description = $data[DESCRIPTION];
+    $csrf = $data['csrf_token'];
+    if (!is_csrf_token_valid("task", $csrf)) {
+        render_not_authorized_json();
+        die;
+    }
+    __validate_task_input($amount, $description);
+    $task_id = dal_task_create($customer_id, $amount, $description);
+    $task = dal_task_fetch($task_id);
+    if (!$task) {
+        render_bad_request_json(['error' => get_db_errors()]);
+        die;
+    } else {
+        send_index_event($task[ID], TASK_DESCRIPTION_IDX, $task[DESCRIPTION]);
+        $response = [
+            'data' => [
+                'task' => $task
+            ],
+            'event' => [
+                [
+                    'render_data' => 'task'
+                ]
+            ]
+        ];
+        render_ok_json($response);
+        return true;
+    }
+}
+
+route_request($routes);
---
+++ src/app/dal/task.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * @author dimyriy
+ * @version 1.0
+ */
+require_once "../bootstrap.php";
+require_once "dal_helper.php";
+$task_connection = null;
+
+function get_task_connection()
+{
+    global $task_connection;
+    if ($task_connection === null) {
+        $task_connection = get_mysqli_connection(TASK_DB);
+    }
+    return $task_connection;
+}
+
+function dal_task_create($customer_id, $amount, $description)
+{
+    $db_errors = initialize_db_errors();
+    $connection = get_task_connection();
+    if (!$connection) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    mysqli_autocommit($connection, false);
+    $task_tx_started = mysqli_begin_transaction($connection);
+    if (!$task_tx_started) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+
+    $stmt = mysqli_prepare($connection, "INSERT INTO db_task.task (customer_id, amount, description) VALUES (?, ?, ?)");
+    if (!$stmt) {
+        add_error(mysqli_error($connection), $db_errors);
+        mysqli_rollback($connection);
+        return false;
+    }
+    /** @noinspection PhpMethodParametersCountMismatchInspection */
+    if (!mysqli_stmt_bind_param($stmt, 'ids', $customer_id, $amount, $description)) {
+        add_error(mysqli_error($connection), $db_errors);
+        mysqli_rollback($connection);
+        return false;
+    }
+    if (!mysqli_stmt_execute($stmt)) {
+        add_error(mysqli_error($connection), $db_errors);
+        mysqli_rollback($connection);
+        return false;
+    }
+    if (!mysqli_stmt_close($stmt)) {
+        add_error(mysqli_error($connection), $db_errors);
+        mysqli_rollback($connection);
+        return false;
+    }
+    if (mysqli_errno($connection) !== 0) {
+        mysqli_rollback($connection);
+        return false;
+    }
+    $task_id = mysqli_insert_id($connection);
+    if (!mysqli_commit($connection)) {
+        return false;
+    }
+    return $task_id;
+}
+
+function dal_task_fetch($task_id)
+{
+    $db_errors = initialize_db_errors();
+    $connection = get_task_connection();
+    if (!$connection) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    $stmt = mysqli_prepare($connection, "SELECT id, created_at, customer_id, performer_id, amount, description FROM db_task.task WHERE id=?");
+    if (!$stmt) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    /** @noinspection PhpMethodParametersCountMismatchInspection */
+    if (!mysqli_stmt_bind_param($stmt, 'i', $task_id)) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    if (!mysqli_stmt_execute($stmt)) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    if (!mysqli_stmt_store_result($stmt)) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    if (mysqli_stmt_num_rows($stmt) < 1) {
+        return null;
+    }
+    if (!mysqli_stmt_bind_result($stmt, $id, $created_at, $customer_id, $performer_id, $amount, $description)) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    if (!mysqli_stmt_fetch($stmt)) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    if (!mysqli_stmt_close($stmt)) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    if (mysqli_errno($connection) !== 0) {
+        return false;
+    }
+    return [
+        ID => $id,
+        DESCRIPTION => $description,
+        CUSTOMER_ID => $customer_id,
+        PERFORMER_ID => $performer_id,
+        CREATED_AT => $created_at,
+        AMOUNT => $amount
+    ];
+}
+
+function dal_task_fetch_all_tasks($user_id, $user_role, $limit = 100, $last_id = null)
+{
+    $db_errors = initialize_db_errors();
+    $connection = get_task_connection();
+    if (!$connection) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    $last_id_clause = $last_id === null ? '' : "AND id < $last_id";
+    if ($user_id === null) {
+        $stmt = mysqli_prepare($connection, "SELECT id, created_at, customer_id, performer_id, amount, description FROM db_task.task WHERE performer_id IS NULL $last_id_clause ORDER BY id DESC LIMIT ?");
+        if (!$stmt) {
+            add_error(mysqli_error($connection), $db_errors);
+            return false;
+        }
+    } else {
+        $stmt = null;
+        if (is_performer($user_role)) {
+            $stmt = mysqli_prepare($connection, "SELECT id, created_at, customer_id, performer_id, amount, description FROM db_task.task WHERE performer_id = ? $last_id_clause ORDER BY ID DESC LIMIT ?");
+        } elseif (is_customer($user_role)) {
+            $stmt = mysqli_prepare($connection, "SELECT id, created_at, customer_id, performer_id, amount, description FROM db_task.task WHERE customer_id = ? $last_id_clause ORDER BY ID DESC LIMIT ?");
+        } else {
+            $stmt = mysqli_prepare($connection, "SELECT id, created_at, customer_id, performer_id, amount, description FROM db_task.task WHERE TRUE $last_id_clause ORDER BY ID DESC LIMIT ?");
+        }
+        if (!$stmt) {
+            add_error(mysqli_error($connection), $db_errors);
+            return false;
+        }
+        /** @noinspection PhpMethodParametersCountMismatchInspection */
+        if (!mysqli_stmt_bind_param($stmt, 'ii', $user_id, $limit)) {
+            add_error(mysqli_error($connection), $db_errors);
+            return false;
+        }
+    }
+    if (!mysqli_stmt_execute($stmt)) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    if (!mysqli_stmt_bind_result($stmt, $id, $created_at, $customer_id, $performer_id, $amount, $description)) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    $json = [];
+    while ($row = mysqli_stmt_fetch($stmt)) {
+        $json[] = [
+            ID => $id,
+            CREATED_AT => $created_at,
+            CUSTOMER_ID => $customer_id,
+            PERFORMER_ID => $performer_id,
+            AMOUNT => $amount,
+            DESCRIPTION => $description
+        ];
+    }
+    if (!mysqli_stmt_close($stmt)) {
+        add_error(mysqli_error($connection), $db_errors);
+        return false;
+    }
+    return $json;
+}
