6ee9b6f9ae59195342c7444a761f15ffebd1206f
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-25 01:32:33 +0300

    - Add type for signup form

--- src/app/view/templates/signup_form.html.php
+++ src/app/view/templates/signup_form.html.php
@@ -1,4 +1,4 @@
-<div id="signup-form-modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+<div id="signup-form-modal" class="modal fade" data-type="signup" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
