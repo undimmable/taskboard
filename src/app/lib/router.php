4a7df606b0b611b1e823c6fdcb0022626256559c
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-28 00:31:49 +0300

    - Refactor router

--- src/app/lib/router.php
+++ src/app/lib/router.php
@@ -3,6 +3,7 @@
 function route_request($routes)
 {
     ob_start();
+    $rendered = false;
     $request_method = $_SERVER["REQUEST_METHOD"];
     if (is_null($request_method) || !isset($routes[$request_method]))
         render_not_allowed_json();
@@ -11,18 +12,21 @@ function route_request($routes)
     if (!is_null($path_param)) {
         if (array_key_exists($path_param, $routes[$request_method])) {
             if (function_exists($routes[$request_method][$path_param])) {
-                return call_user_func($routes[$request_method][$path_param]);
+                call_user_func($routes[$request_method][$path_param]);
+                $rendered = true;
             }
         } else if (array_key_exists('/\d+/', $routes[$request_method])) {
             if (preg_match('/\d+/', $path_param)) {
                 $id = $path_param;
                 if (function_exists($routes[$request_method]['/\d+/'])) {
-                    return call_user_func($routes[$request_method]['/\d+/'], $id);
+                    call_user_func($routes[$request_method]['/\d+/'], $id);
+                    $rendered = true;
                 }
             }
         }
     }
-    render_not_found();
+    if (!$rendered)
+        render_not_found();
     ob_end_flush();
-    die();
+    die;
 }
