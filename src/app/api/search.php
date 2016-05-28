c46224e85ffe69f07e119431b49f6265d930a4d9
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-28 18:39:51 +0300

    - Add search api

---
+++ src/app/api/search.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "../bootstrap.php";
+require_once "../dal/text_idx.php";
+$routes = [
+    'POST' => [
+    ],
+    'GET' => [
+        ROOT => 'api_search'
+    ],
+    'PUT' => [],
+    'DELETE' => []
+];
+
+function api_search()
+{
+    $text = $_GET['q'];
+    $object = find_object($text);
+    if (!$object) {
+        render_not_found();
+    } else {
+        echo $object;
+    }
+}
+
+route_request($routes);
