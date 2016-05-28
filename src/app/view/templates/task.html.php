c43f86cedc4ca285471581a242cd8c3b6aea41c3
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-28 18:09:23 +0300

    - Change task feed layout

--- src/app/view/templates/task.html.php
+++ src/app/view/templates/task.html.php
@@ -1,17 +1,35 @@
 <?php
 global $user;
 ?>
+
+
 <li class="task-feed-item media" data-id="<?php /** @noinspection PhpUndefinedVariableInspection */
 echo $task[ID] ?>">
-    <div class="media-body task-item">
-        <p class="task-description"><?php echo htmlspecialchars($task[DESCRIPTION], ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5 | ENT_DISALLOWED, 'UTF-8', false) ?></p>
-        <span class="task-price"><?php echo $task[AMOUNT] ?></span>
-        <span class="timestamp created_at" data-timestamp-offset="<?php echo $task[CREATED_AT_OFFSET] ?>"></span>
-        <?php if (is_customer($user[ROLE])) {
-            echo '<button type="button" class="delete-task btn-danger">Delete</button>';
-        } else if (is_performer($user[ROLE])) {
-            echo '<button type="button" class="perform-task btn-primary">Perform</button>';
-        }
-        ?>
+    <a href="#" class="pull-left">
+        <img class="avatar-img" src="/img/404.jpg" alt="">
+    </a>
+
+    <div class="row media-body task-item">
+
+        <div class="col-md-9">
+            <strong>Customer Name</strong>
+            <p class="task-description"><?php echo htmlspecialchars($task[DESCRIPTION], ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5 | ENT_DISALLOWED, 'UTF-8', false) ?></p>
+        </div>
+
+        <div class="col-md-3">
+            <small class="text-muted pull-right">
+                <span class="timestamp created_at"
+                      data-timestamp-offset="<?php echo $task[CREATED_AT_OFFSET] ?>"></span>
+            </small>
+                        <?php if (is_performer($user[ROLE])) {
+                            echo '<button type="button" class="delete-task btn-primary btn pull-right no-shadow">$' . $task[AMOUNT]. '</button>';
+                        }
+                        ?>
+
+        </div>
     </div>
+<?php if (is_customer($user[ROLE])) {
+    echo '<button type="button" class="delete-task btn-primary btn-sm btn-link pull-right">Delete</button>';
+}
+    ?>
 </li>
