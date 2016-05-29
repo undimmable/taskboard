d4b59b781b5da47407b29f4e561313af2fc028f0
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-29 22:35:59 +0300

    - Add autoload to phpunit

--- tests/init.php
+++ tests/init.php
@@ -1,3 +1,7 @@
 <?php
 ini_set('date.timezone', 'UTC');
+set_include_path(get_include_path() . PATH_SEPARATOR . 'src/');
+set_include_path(get_include_path() . PATH_SEPARATOR . 'vendor/');
+set_include_path(get_include_path() . PATH_SEPARATOR . 'tests/integration/base');
+require_once 'autoload.php';
 $php_config_path = 'tests/config';
