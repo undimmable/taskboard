ab4de8c69f617f53713829d6a892eefff8f7c1b1
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-29 23:53:43 +0300

    - Rewrite tearDown for mysqli

--- tests/integration/base/util.php
+++ tests/integration/base/util.php
@@ -10,7 +10,7 @@ class Util
     /**
      * @var array
      */
-    private $created_entities = [];
+    private $collected_entities = [];
 
     /**
      * @var \mysqli
@@ -24,31 +24,58 @@ class Util
     public function __construct($mysql)
     {
         $this->connection = $mysql;
+        $collect = [
+            "db_account.account" => "user_id",
+            "db_user.user" => "id",
+            "db_tx.tx" => "id",
+            "db_task.task" => "id",
+            "db_login.login" => "user_id",
+            "db_text_idx.text_idx" => "entity_id",
+            "db_user_info.user_info" => "user_id"
+        ];
+        try {
+            foreach ($collect as $collect_db => $collect_column) {
+                $this->collectState($mysql, $collect_db, $collect_column);
+            }
+        } catch (\Exception $e) {
+            echo $e->getTraceAsString();
+        }
+    }
+
+    /**
+     * @param $connection \mysqli
+     * @param $db \string
+     * @param $column \string
+     */
+    private function collectState($connection, $db, $column)
+    {
+        $result = $connection->query("SELECT COALESCE(max($column), -1) as last_id FROM $db");
+        $last_id = -1;
+        if ($result->num_rows > 0) {
+            $last_id = $result->fetch_row()[0];
+        }
+        if (!array_key_exists($db, $this->collected_entities)) {
+            $this->collected_entities[$db] = [];
+        }
+        $this->collected_entities[$db][$column] = $last_id;
     }
 
 
     /**
-     * @param $email string
-     * @param $password string
-     * @param $role integer
+     * @param $email \string
+     * @param $password \string
+     * @param $role \integer
+     * @param $confirmed \bool
      */
     public function createUser($email, $password, $role, $confirmed)
     {
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
         $confirmation_token = hash("sha256", $email . 'confirmation_secret');
         $mysqli_stmt = $this->connection->prepare("INSERT INTO db_user.user (email, hashed_password, role, confirmation_token, confirmed) VALUES (?, ?, ?, ?, ?)");
-        $mysqli_stmt->bind_param("ssisi", $email, $hashed_password, $role, $confirmation_token, $confirmed);
+        $confirmed_int = (integer)$confirmed;
+        $mysqli_stmt->bind_param("ssisi", $email, $hashed_password, $role, $confirmation_token, $confirmed_int);
         $mysqli_stmt->execute();
         $mysqli_stmt->close();
-        $this->putCreatedEntity("db_user.user", $this->connection->insert_id);
-    }
-
-    private function putCreatedEntity($key, $id)
-    {
-        if (!array_key_exists($key, $this->created_entities)) {
-            $this->created_entities[$key] = [];
-        }
-        $this->created_entities[$key][] = $id;
     }
 
     /**
@@ -56,9 +83,11 @@ class Util
      */
     public function deleteAllCreatedEntities()
     {
-        foreach ($this->created_entities as $entity_type => $id_array) {
-            /** @noinspection SqlResolve */
-            $this->connection->query("delete from $entity_type WHERE id in (" . implode(',', $id_array) . ");");
+        foreach ($this->collected_entities as $entity_type => $key_value) {
+            foreach ($key_value as $key => $last_value) {
+                /** @noinspection SqlResolve */
+                $this->connection->query("delete from $entity_type WHERE $key > $last_value");
+            }
         }
     }
 }
