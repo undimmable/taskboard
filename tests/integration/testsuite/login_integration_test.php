93e5cd4272bbc365571ec99f59cea0e5e393c269
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-29 22:36:50 +0300

    - Add loginIntegrationTest

---
+++ tests/integration/testsuite/login_integration_test.php
@@ -0,0 +1,62 @@
+<?php
+namespace Taskboards;
+require 'api_integration_test.php';
+
+class LoginIntegrationTest extends ApiIntegrationTest
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->util->createUser("dummy@dummy.com", "123456", 2, 0);
+    }
+
+    public function testLoginExistingUserShouldReturnToken()
+    {
+        $credentials = [
+            'email' => 'dummy@dummy.com',
+            'password' => '123456',
+            'csrf_token' => '9'
+        ];
+        $response = $this->api->post('auth/login', ['form_params' => $credentials]);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testLoginMissingUserShouldReturnUnauthorized()
+    {
+        $credentials = [
+            'email' => 'missing@dummy.com',
+            'password' => '123456',
+            'csrf_token' => '9'
+        ];
+        $response = $this->api->post('auth/login', ['form_params' => $credentials]);
+        $this->assertEquals(401, $response->getStatusCode());
+    }
+
+    public function testLoginExistingUserWrongTokenShouldReturnUnauthorized()
+    {
+        $credentials = [
+            'email' => 'dummy@dummy.com',
+            'password' => '123456',
+            'csrf_token' => '5'
+        ];
+        $response = $this->api->post('auth/login', ['form_params' => $credentials]);
+        $this->assertEquals(401, $response->getStatusCode());
+    }
+
+    public function testLoginMissingUserWrongTokenShouldReturnUnauthorized()
+    {
+        $credentials = [
+            'email' => 'missing@dummy.com',
+            'password' => '123456',
+            'csrf_token' => '5'
+        ];
+        $response = $this->api->post('auth/login', ['form_params' => $credentials]);
+        $this->assertEquals(401, $response->getStatusCode());
+    }
+
+    public function tearDown()
+    {
+        $this->util->deleteAllCreatedEntities();
+        parent::tearDown();
+    }
+}
