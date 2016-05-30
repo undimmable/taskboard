9f66cd3f0333f5c377cbac0865cda7762669159d
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-30 22:35:48 +0300

    - Add lock_tx_id to task creation sql

--- config/db/task.sql
+++ config/db/task.sql
@@ -3,12 +3,13 @@ CREATE DATABASE db_task;
 GRANT ALL ON db_task.* TO 'user_task'@'%';
 USE db_task;
 CREATE TABLE task (
-  id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+  id           BIGINT PRIMARY KEY               AUTO_INCREMENT,
   created_at   TIMESTAMP DEFAULT now() NOT NULL,
   customer_id  BIGINT                  NOT NULL,
-  performer_id BIGINT             DEFAULT NULL,
+  performer_id BIGINT                           DEFAULT NULL,
   amount       NUMERIC(10, 2)          NOT NULL,
   description  MEDIUMTEXT              NOT NULL,
+  lock_tx_id   BIGINT                  NOT NULL DEFAULT -1,
   deleted      BOOL DEFAULT FALSE      NOT NULL
 )
   ENGINE = InnoDB
