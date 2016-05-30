f2b373ae9485dfd7ca3368078e024f6497359eda
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-05-30 21:29:30 +0300

    - Add failed attempts

--- config/db/login.sql
+++ config/db/login.sql
@@ -3,11 +3,12 @@ CREATE DATABASE db_login;
 GRANT ALL ON db_login.* TO 'user_login'@'%';
 USE db_login;
 CREATE TABLE login (
-  id          BIGINT PRIMARY KEY AUTO_INCREMENT,
-  user_id     BIGINT             DEFAULT NULL,
-  ip          VARBINARY(16)           NOT NULL,
-  user_client VARCHAR(31)             NOT NULL,
-  created_at  TIMESTAMP DEFAULT now() NOT NULL
+  id             BIGINT PRIMARY KEY AUTO_INCREMENT,
+  user_id        BIGINT             DEFAULT NULL,
+  ip             VARBINARY(16)           NOT NULL,
+  user_client    VARCHAR(31)             NOT NULL,
+  created_at     TIMESTAMP DEFAULT now() NOT NULL,
+  failed_attepts TINYINT            DEFAULT NULL
 )
   ENGINE = InnoDB
   DEFAULT CHARSET UTF8;
