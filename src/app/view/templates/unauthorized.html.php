d1ef0620ff1502ab8a551e780253e07a3bf277c9
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-06-05 22:29:24 +0300

    - Add unauthorized page

--- src/app/view/templates/unauthorized.html.php
+++ src/app/view/templates/unauthorized.html.php
@@ -1,1 +1,162 @@
-Unauthorized
+<div class="col-lg-8 col-md-10 col-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-1 col-sm-offset-0 col-xs-offset-0">
+    <div class="feed-wrapper">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                Task feed
+            </div>
+            <div class="panel-body">
+                <ul id="task-feed" class="media-list">
+                    <li class="task-feed-item media task-active">
+                        <a href="#" class="pull-left">
+                            <img class="avatar-img" src="/img/w.png">
+                        </a>
+                        <div class="row media-body task-item">
+                            <div class="col-md-9">
+                                <strong class="task-header">Jean Customer</strong>
+                                <p class="task-description">In ornare venenatis nibh nec eleifend. Vestibulum eget
+                                    vulputate lacus. Sed ultricies dolor felis, id dapibus lorem feugiat a. In cursus
+                                    auctor lectus, facilisis hendrerit ligula volutpat sit amet. Sed sagittis venenatis
+                                    tortor quis placerat. Proin ac orci ac eros ultrices venenatis in eget justo. Donec
+                                    blandit ipsum sed nibh pretium porta sed in justo.
+                                </p>
+                            </div>
+                            <div class="col-md-3">
+                                <small class="text-muted pull-right">
+                                    <span class="timestamp created_at"
+                                          data-timestamp-offset="-3432">about an hour ago</span>
+                                </small>
+                                <button type="button" class="perform-task btn btn-primary pull-right no-shadow"
+                                        data-toggle="modal" data-target="#signup-form-modal">
+                                    $100.00
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="task-feed-item media task-active">
+                        <a href="#" class="pull-left">
+                            <img class="avatar-img" src="/img/w.png">
+                        </a>
+                        <div class="row media-body task-item">
+                            <div class="col-md-9">
+                                <strong class="task-header">Jean Customer</strong>
+                                <p class="task-description">Mauris nunc justo, feugiat vitae viverra non, gravida ut
+                                    tortor. Proin tempus leo sem, vel rutrum purus tincidunt id. Integer at eros
+                                    eleifend, aliquet lectus quis, feugiat turpis.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <small class="text-muted pull-right">
+                                    <span class="timestamp created_at"
+                                          data-timestamp-offset="-3438">about an hour ago</span>
+                                </small>
+                                <button type="button" class="perform-task btn btn-primary pull-right no-shadow"
+                                        data-toggle="modal" data-target="#signup-form-modal">$50.00
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="task-feed-item media task-active">
+                        <a href="#" class="pull-left">
+                            <img class="avatar-img" src="/img/w.png">
+                        </a>
+                        <div class="row media-body task-item">
+                            <div class="col-md-9">
+                                <strong class="task-header">Jean Customer</strong>
+                                <p class="task-description">Praesent scelerisque scelerisque ornare. Duis fermentum
+                                    vehicula nulla, ac convallis mi bibendum nec. Integer sapien nibh, laoreet sed
+                                    placerat eget, tincidunt eget mauris. Phasellus mauris velit, sagittis tincidunt
+                                    molestie eu, dignissim et ex.
+                                </p>
+                            </div>
+                            <div class="col-md-3">
+                                <small class="text-muted pull-right">
+                                    <span class="timestamp created_at"
+                                          data-timestamp-offset="-3456">about an hour ago</span>
+                                </small>
+                                <button type="button" class="perform-task btn btn-primary pull-right no-shadow"
+                                        data-toggle="modal" data-target="#signup-form-modal">
+                                    $12.00
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="task-feed-item media task-active">
+                        <a href="#" class="pull-left">
+                            <img class="avatar-img" src="/img/w.png">
+                        </a>
+                        <div class="row media-body task-item">
+                            <div class="col-md-9">
+                                <strong class="task-header">Jean Customer</strong>
+                                <p class="task-description">Fusce tristique nibh nisi, ac ullamcorper massa suscipit at.
+                                </p>
+                            </div>
+                            <div class="col-md-3">
+                                <small class="text-muted pull-right">
+                                    <span class="timestamp created_at"
+                                          data-timestamp-offset="-3521">about an hour ago</span>
+                                </small>
+                                <button type="button" class="perform-task btn btn-primary pull-right no-shadow"
+                                        data-toggle="modal" data-target="#signup-form-modal">
+                                    $10.00
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="task-feed-item media task-active">
+                        <a href="#" class="pull-left">
+                            <img class="avatar-img" src="/img/w.png">
+                        </a>
+                        <div class="row media-body task-item">
+                            <div class="col-md-9">
+                                <strong class="task-header">Jean Customer</strong>
+                                <p class="task-description">Vivamus euismod bibendum lorem vitae imperdiet. Nam luctus
+                                    urna sed quam ornare suscipit. Fusce ut consequat velit. In consectetur mi leo, sit
+                                    amet ultricies nunc dignissim in. Cum sociis natoque penatibus et magnis dis
+                                    parturient montes, nascetur ridiculus mus. Vestibulum iaculis gravida fringilla.
+                                    Nullam laoreet pretium eros et rhoncus.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <small class="text-muted pull-right">
+                                    <span class="timestamp created_at"
+                                          data-timestamp-offset="-3678">about an hour ago</span>
+                                </small>
+                                <button type="button" class="perform-task btn btn-primary pull-right no-shadow"
+                                        data-toggle="modal" data-target="#signup-form-modal">
+                                    $12.00
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="task-feed-item media task-active" data-id="23">
+                        <a href="#" class="pull-left">
+                            <img class="avatar-img" src="/img/w.png">
+                        </a>
+                        <div class="row media-body task-item">
+                            <div class="col-md-9">
+                                <strong class="task-header">Jean Customer</strong>
+                                <p class="task-description">Vivamus dictum, velit eu tempor bibendum, leo augue lacinia
+                                    massa, id convallis nulla orci a justo. Nulla facilisi. Ut pellentesque sapien at
+                                    odio dapibus, quis posuere turpis rutrum.
+                                </p>
+                            </div>
+                            <div class="col-md-3">
+                                <small class="text-muted pull-right">
+                                    <span class="timestamp created_at"
+                                          data-timestamp-offset="-3964">about an hour ago</span>
+                                </small>
+                                <button type="button" class="perform-task btn btn-primary pull-right no-shadow"
+                                        data-toggle="modal" data-target="#signup-form-modal">$2.00
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div id="loading" class="panel-footer" hidden="" style="display: none;">
+                Loading...
+            </div>
+            <div id="no-more-content" class="panel-footer">
+                No more content
+            </div>
+        </div>
+    </div>
+</div>
