e5922acdc3710de8cf42831fad37d01702fc082b
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-06-10 10:07:12 +0300

    - Add limits checking to php code as well

---
+++ src/app/lib/limits.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author dimyriy
+ * @version 1.0
+ */
+
+$_max_connections_per_ip = 10;
+$_max_requests = 10;
+$_request_limit_timeout_sec = 60;
+$_ip_storage = [];
+$_requests_storage = [];
+
+/**
+ * Check if ip has reached the maximum connections limit
+ * @param $ip
+ * @return bool
+ */
+function check_ip_limits($ip)
+{
+    global $_max_connections_per_ip;
+    if (empty($ip)) {
+        return false;
+    }
+    if (!isset ($__ipStorage[$ip])) {
+        return true;
+    }
+    return ($__ipStorage[$ip] > $_max_connections_per_ip) ? false : true;
+}
+
+function check_request_limits($client_id)
+{
+    global $_requests_storage, $_request_limit_timeout_sec, $_max_requests;
+    if (array_key_exists($client_id, $_requests_storage)) {
+        $_requests_storage[$client_id] = ['last' => time(), 'total' => 1];
+        return true;
+    }
+
+    if (time() - $_requests_storage[$client_id]['last'] > $_request_limit_timeout_sec) {
+        $_request_storage[$client_id] = ['last' => time(), 'total' => 1];
+        return true;
+    }
+    if ($_requests_storage[$client_id]['total'] > $_max_requests) {
+        return false;
+    }
+    $_requests_storage[$client_id]['total']++;
+    return true;
+
+}
