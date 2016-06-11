7bcb8d7d294d8c13a04ec2363deeb444f9763b08
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-06-12 02:29:18 +0300

    - Add paid to constants

--- src/app/config/constants.php
+++ src/app/config/constants.php
@@ -35,6 +35,7 @@ define('ACCESS_TOKEN', 'access_token');
 define('HASHED_PASSWORD', 'hashed_password');
 define('DESCRIPTION', 'description');
 define('AMOUNT', 'amount');
+define('PAID', 'paid');
 define('GET', 'GET');
 define('POST', 'POST');
 define('PUT', 'PUT');
