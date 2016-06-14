3765f8469ed744c38cab82f1170a07bae3790d5b
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-06-14 12:57:47 +0300

    - Remove index_event

--- src/app/events/event.php
+++ src/app/events/event.php
@@ -21,20 +21,5 @@
  */
 function send_generic_event($entity_id, $json, $type)
 {
-    //do nothing
-}
-
-/**
- * @param $entity_id        integer Id of an entity
- * @param $entity_type      string Type of an entity
- * @param $text             string Text to index
- */
-function send_index_event($entity_id, $entity_type, $text)
-{
-    $json = json_encode([
-        'entity_id' => $entity_id,
-        'entity_type' => $entity_type,
-        'text' => $text
-    ]);
-    send_generic_event($entity_id, $json, 'text_idx');
+    
 }
