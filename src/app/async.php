b18022b189ad2399a3492ae9b2ded09a9dee2b24
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-06-15 01:24:35 +0300

    - Fix token check in async

--- src/app/async.php
+++ src/app/async.php
@@ -9,25 +9,40 @@ set_include_path(get_include_path() . PATH_SEPARATOR . '/var/www/taskboard_confi
 set_include_path(get_include_path() . PATH_SEPARATOR . '/Users/dimyriy/Development/projects/vk/taskboard/src/app');
 set_include_path(get_include_path() . PATH_SEPARATOR . '/Users/dimyriy/Development/projects/vk/taskboard/config/php');
 require('bootstrap.php');
+date_default_timezone_set('UTC');
 $master = null;
 $clients = [];
+$forbidden_clients = [];
 $clients_size = 0;
 $critical_client_size = 500;
+$debug_enabled = true;
 
-function str_from_mem(&$value)
+function log_msg($msg, $file)
 {
-    $i = strpos($value, "\0");
-    if ($i === false) {
-        return $value;
-    }
-    $result = substr($value, 0, $i);
-    return $result;
+    error_log("[" . date('Y-m-d H:m:s:u') . "]" . $msg . "\n", 3, $file);
+}
+
+function log_info($msg)
+{
+    log_msg($msg, "/var/log/async_php_access.log");
+}
+
+function log_error($msg)
+{
+    log_msg($msg, "/var/log/async_php_error.log");
+}
+
+function log_debug($msg)
+{
+    global $debug_enabled;
+    if ($debug_enabled)
+        log_msg($msg, "/var/log/async_php_debug.log");
 }
 
 function send_event_to_client($client, $str)
 {
     $event = 'data:' . $str . "\n\n";
-    print("Sending event " . $event);
+    log_debug("Sending event " . $event . " to " . $client[USER_ID]);
     if (!is_resource($client['connection'])) {
         drop_client($client);
         return;
@@ -36,7 +51,7 @@ function send_event_to_client($client, $str)
         echo "Socket write has failed " . socket_strerror(socket_last_error());
         drop_client($client);
     }
-    print("Event sent " . $event);
+    log_debug("Event " . $event . " sent to client " . $client[USER_ID]);
 }
 
 function drop_client($client)
@@ -58,6 +73,8 @@ function drop_client($client)
 function add_client($client)
 {
     global $clients_size, $master, $clients, $critical_client_size;
+    $user_id = $client[USER_ID];
+    print("Connected client $user_id");
     if (socket_last_error($master)) {
         socket_clear_error($master);
     } else {
@@ -65,8 +82,8 @@ function add_client($client)
             @socket_close($client['connection']);
         } else {
             $clients_size++;
-            if (!array_key_exists($client[USER_ID], $clients)) {
-                $clients[$client[USER_ID]] = [];
+            if (!array_key_exists($user_id, $clients)) {
+                $clients[$user_id] = [];
             }
             $clients[USER_ID][] = $client;
             @socket_write($client['connection'], "HTTP/1.1 200 OK\r\nContent-Type: text/event-stream\r\nCache-Control: no-cache\r\nConnection: keep-alive\r\nX-Frame-Options: SAMEORIGIN\r\nX-Xss-Protection:1; mode=block\r\nX-Content-Type-Options: nosniff\r\n\r\n");
@@ -77,7 +94,10 @@ function add_client($client)
 function parse_client($connection)
 {
     $read = socket_recv($connection, $request, 2048, MSG_DONTWAIT);
+    log_debug($read);
+    log_debug($request);
     if (!$read) {
+        log_error("Couldn't parse client headers, will drop");
         return false;
     }
     $lines = explode("\n", $request);
@@ -85,9 +105,13 @@ function parse_client($connection)
     $headers = [];
     $auth_cookie = null;
     if (strpos($request, get_event_token_header()) < 0) {
-        return false;
+        log_error("Request from unknown source, will drop");
+        return null;
     }
     $event_csrf = null;
+    $client_snapshot_timestamp = null;
+    $user_agent = null;
+    $client_ip = null;
     foreach ($lines as $line) {
         $line = trim($line);
         if (strpos($line, ': ') !== false) {
@@ -101,81 +125,108 @@ function parse_client($connection)
             }
             if ($key == 'X-CSRF-TOKEN') {
                 $event_csrf = $value;
+            } else if ($key == 'X-CURRENT-SNAPSHOT-TIMESTAMP') {
+                $client_snapshot_timestamp = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+            } else if ($key == 'X-Real-IP') {
+                $client_ip = filter_var($value, FILTER_SANITIZE_STRING);
+            } else if ($key == 'User-Agent') {
+                $user_agent = filter_var($value, FILTER_SANITIZE_STRING);
             }
             $headers[$key] = $value;
         }
     }
     if (is_null($event_csrf)) {
-        return false;
+        log_info("Couldn't parse event token, will drop");
+        return null;
     }
-    $_SERVER['REMOTE_ADDR'] = array_key_exists('X-Real-IP', $headers) ? $headers['X-Real-IP'] : '';
-    $_SERVER['HTTP_USER_AGENT'] = array_key_exists('User-Agent', $headers) ? $headers['User-Agent'] : '';
-    if ($auth_cookie) {
-        $user = parse_user_from_token(parse_token_from_string($auth_cookie));
-        if (is_null($user) || !array_key_exists(ID, $user))
-            return false;
-        if ($event_csrf != get_event_csrf($user[ID], get_secrets_payload($user))) {
-            return false;
+    if (is_null($client_snapshot_timestamp)) {
+        log_info("Client hasn't provided snapshot timestamp, will drop");
+        return null;
+    }
+    if (is_null($client_ip)) {
+        log_info("Client hasn't provided ip, will drop");
+        return null;
+    }
+    if (is_null($user_agent)) {
+        log_info("Client hasn't provided user-agent, will drop");
+        return null;
+    }
+    if (is_null($auth_cookie)) {
+        log_info("Client hasn't provided authentication cookie, will drop");
+        return null;
+    }
+    $_SERVER['REMOTE_ADDR'] = $client_ip;
+    $_SERVER['HTTP_USER_AGENT'] = $user_agent;
+    $user = parse_user_from_token(parse_token_from_string($auth_cookie));
+    if (is_null($user) || !array_key_exists(ID, $user)) {
+        log_info("Client provided malformed token, will drop");
+        return null;
+    }
+    $user_id = $user[ID];
+    if ($event_csrf != get_event_csrf($user_id, get_secrets_payload($user_id))) {
+        log_info("Client provided wrong token $event_csrf, will drop");
+        return null;
+    }
+    $login = dal_login_fetch($user[ID], parse_ip(), parse_user_client());
+    if (!$login) {
+        log_info("Client provided token for unauthenticated user $user_id, will drop");
+        return null;
+    }
+    return [
+        'user_id' => $user_id,
+        'connection' => $connection,
+        'user_email' => $user[EMAIL],
+        'last_event_timestamp' => $client_snapshot_timestamp
+    ];
+}
+
+function fetch_events()
+{
+    global $clients;
+    foreach ($clients as $user_id => $client_array) {
+        foreach ($client_array as $existing_client) {
+            $id = $existing_client[USER_ID];
+            $shm_id = shm_attach($id, 1024);
+            if (($var = shm_get_var($shm_id, 0))) {
+                send_event_to_client($existing_client, $var);
+            }
         }
-        $login = dal_login_fetch($user[ID], parse_ip(), parse_user_client());
-        if (!$login)
-            return false;
-        return ['method' => strtoupper($method), 'uri' => $uri, 'headers' => $headers, 'user_id' => $user[ID], 'connection' => $connection, 'user_email' => $user[EMAIL], 'last_event_timestamp' => $headers['X-CURRENT-SNAPSHOT-TIMESTAMP']];
-    } else {
-        return false;
     }
 }
 
-//$context = stream_context_create(
-//    [
-//        'ssl' => [
-//            'local_cert' => '/etc/ssl/taskboards.top.cert',
-//            'allow_self_signed' => true,
-//            'disable_compression' => true,
-//            'verify_peer' => false,
-//            'ciphers' => 'ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4',
-//
-//        ]
-//    ]
-//);
 $socket_file = "/var/www/taskboards-events.sock";
 $master = socket_create(AF_UNIX, SOCK_STREAM, 0);
 if (!$master) {
-    error_log("Couldn't create socket, " . socket_strerror(socket_last_error($master)));
+    log_error("Couldn't create socket, dying. " . socket_strerror(socket_last_error($master)));
     die(2);
 }
 @unlink($socket_file);
 if (!@socket_bind($master, $socket_file)) {
-    error_log("Couldn't bind socket, " . socket_strerror(socket_last_error($master)));
+    log_error("Couldn't bind socket, dying. " . socket_strerror(socket_last_error($master)));
     die(2);
 }
-@socket_set_nonblock($master);
-@socket_listen($master, $critical_client_size);
-$iter = 0;
+if (!socket_set_nonblock($master)) {
+    log_error("Couldn't set socket non blocking, switching to blocking io. " . socket_strerror(socket_last_error($master)));
+}
+if (!socket_listen($master, $critical_client_size)) {
+    log_error("Couldn't listen on socket, dying. " . socket_strerror(socket_last_error($master)));
+    die(2);
+}
 for (; ;) {
     $connection = socket_accept($master);
-    $iter++;
+    log_info("Socket accepted incoming connection " . $connection);
     if ($connection) {
-        $client = parse_client($connection);
-        if ($client) {
-            add_client($client);
+        $incoming_client = parse_client($connection);
+        $client_str = print_r($incoming_client, true);
+        log_info("Parsed client $client_str");
+        if ($incoming_client) {
+            add_client($incoming_client);
         } else {
+            log_info("Dropping client $client_str due to previous errors");
             @socket_close($connection);
         }
     }
-    if ($iter == 10) {
-        $iter = 0;
-        foreach ($clients as $user_id => $client) {
-            foreach ($client as $cl) {
-                $id = $cl[USER_ID];
-                $shm_id = shm_attach($id, 1024);
-                if (($var = shm_get_var($shm_id, 0))) {
-                    var_dump($var);
-                    send_event_to_client($cl, $var);
-                }
-            }
-        }
-    }
+    fetch_events();
     sleep(1);
 }
 @socket_close($master);
