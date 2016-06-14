99e6acd32d6d6863bfd0c24ce4499c62243da781
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-06-14 13:28:46 +0300

    - Add getters for events secrets

--- config/php/taskboard_security_config.php
+++ config/php/taskboard_security_config.php
@@ -2,8 +2,7 @@
 return [
     'jwt_secret'            => 'rplc_jwt_secret',
     'confirmation_secret'   => 'rplc_confirmation_key',
-    'vk_client_id'          => 'rplc_vk_client_id',
-    'vk_secret'             => 'rplc_vk_secret',
+    'events_secret'         => 'rplc_events_key',
     'task_csrf_secret'      => 'rplc_task_csrf_secret',
     'login_csrf_secret'     => 'rplc_login_csrf_secret',
     'account_csrf_secret'   => 'rplc_account_csrf_secret',
