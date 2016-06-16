87a61491171ef1dd6938b09dabae3b011aed888c
dmbogdanov <dimyriy.bogdanov@gmail.com>
2016-06-16 22:31:15 +0300

    - Allow nulls in event table

--- config/db/event.sql
+++ config/db/event.sql
@@ -5,7 +5,7 @@ USE db_event;
 
 CREATE TABLE event (
   id         BIGINT        NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  target_id  BIGINT        NOT NULL,
+  target_id  BIGINT,
   message    VARCHAR(2048) NOT NULL,
   created_at TIMESTAMP(3)  NOT NULL             DEFAULT CURRENT_TIMESTAMP(3),
   type       CHAR(1)       NOT NULL             DEFAULT 'c'
