87ea57e4dbeb6840fa615092490e0bdef71676bd
Dmitry Bogdanov <dimyriy.bogdanov@gmail.com>
2016-06-20 17:14:59 +0300

    Closes #17

--- config/db/event.sql
+++ config/db/event.sql
@@ -6,6 +6,7 @@ USE db_event;
 CREATE TABLE event (
   id         BIGINT        NOT NULL PRIMARY KEY AUTO_INCREMENT,
   target_id  BIGINT,
+  target_role TINYINT NOT NULL DEFAULT 4,
   message    VARCHAR(2048) NOT NULL,
   created_at TIMESTAMP(3)  NOT NULL             DEFAULT CURRENT_TIMESTAMP(3),
   type       CHAR(1)       NOT NULL             DEFAULT 'c',
